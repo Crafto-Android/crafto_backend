d119ab24535ca97bddc93e2eab4317a177354db6
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-09-22 18:03:29 +0300

    feat: add request body annotation

--- src/main/kotlin/com/crafto/crafto_backend/controller/CustomerController.kt
+++ src/main/kotlin/com/crafto/crafto_backend/controller/CustomerController.kt
@@ -3,6 +3,7 @@ package com.crafto.crafto_backend.controller
 import com.crafto.crafto_backend.request.CustomerRequest
 import com.crafto.crafto_backend.service.CustomerService
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,5 +14,5 @@ import org.springframework.web.bind.annotation.RestController
 class CustomerController(private val customerService: CustomerService) {
 
     @PostMapping
-    fun getCustomerById(body: CustomerRequest) = customerService.saveCustomer(body = body)
+    fun getCustomerById(@RequestBody body: CustomerRequest) = customerService.saveCustomer(body = body)
 }
