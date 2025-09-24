17dff5e2eab117589240352ecaa08082c7851a49
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-09-24 08:39:15 +0300

    add:authentication model

---
+++ src/main/kotlin/com/crafto/crafto_backend/entity/Authentication.kt
@@ -0,0 +1,11 @@
+package com.crafto.crafto_backend.entity
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document("Authentication")
+data class Authentication(
+    @Id val userId: ObjectId,
+    val isCustomer: Boolean,
+)
