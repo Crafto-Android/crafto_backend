b5373649e76f96ebdaaab00eba5fb758b15e43ac
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-09-24 08:41:48 +0300

    add:properties and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.session:spring-session-data-mongodb")
