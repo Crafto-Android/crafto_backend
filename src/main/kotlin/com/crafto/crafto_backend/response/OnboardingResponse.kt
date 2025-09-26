7fa3546302838e430e2923a643cf19154c2584fa
Hend Sayed <hendsayed853@gmail.com>
2025-09-26 21:46:15 +0300

    remove unused file

--- src/main/kotlin/com/crafto/crafto_backend/response/OnboardingResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.crafto.crafto_backend.response
-
-data class OnboardingResponse (
-    val id : String,
-    val imageUrl : String,
-    val title : String,
-    val description : String
-)// will be edited
