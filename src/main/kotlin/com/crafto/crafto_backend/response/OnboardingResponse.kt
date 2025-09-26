1e2a4809cdbbf91708375abde59193ef1528fd4b
Hend Sayed <hendsayed853@gmail.com>
2025-09-26 12:30:22 +0300

    create onboarding end point

---
+++ src/main/kotlin/com/crafto/crafto_backend/response/OnboardingResponse.kt
@@ -0,0 +1,8 @@
+package com.crafto.crafto_backend.response
+
+data class OnboardingResponse (
+    val id : String,
+    val imageUrl : String,
+    val title : String,
+    val description : String
+)// will be edited
