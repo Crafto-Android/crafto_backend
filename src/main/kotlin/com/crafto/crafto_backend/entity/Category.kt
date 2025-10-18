507090bf7292b6e0efad8b7f6dc5e61bba350c81
Hend Sayed <hendsayed853@gmail.com>
2025-10-18 21:39:32 +0300

    create search endpoint for categories

--- src/main/kotlin/com/crafto/crafto_backend/entity/Category.kt
+++ src/main/kotlin/com/crafto/crafto_backend/entity/Category.kt
@@ -1,11 +1,13 @@
 package com.crafto.crafto_backend.entity
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.TextIndexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document("categories")
 data class Category(
     @Id
     val id: String,
+    @TextIndexed
     val categoryName:String,
     val categoryColor:String
 )
--- src/main/kotlin/com/crafto/crafto_backend/service/SharedService.kt
+++ src/main/kotlin/com/crafto/crafto_backend/service/SharedService.kt
@@ -3,12 +3,27 @@ package com.crafto.crafto_backend.service
 import com.crafto.crafto_backend.mapper.toCategoryResponse
 import com.crafto.crafto_backend.repository.CategoryRepository
 import com.crafto.crafto_backend.dto.CategoryResponse
+import com.crafto.crafto_backend.entity.Category
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Service
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+
 
 @Service
-class SharedService (private val repository: CategoryRepository){
+class SharedService (
+    private val repository: CategoryRepository,
+    private val mongoTemplate: MongoTemplate
+){
 
     fun getAllCategories():List<CategoryResponse>{
         return repository.findAll().map { it.toCategoryResponse() }
+    }
+
+    fun search(query:String) : List<Category>{
+        val pattern=".*${Regex.escape(query)}.*"
+        val criteria = Criteria.where("categoryName").regex(pattern,"i") // i-> refer to ignore case-insensitive
+        val q = Query(criteria)
+        return mongoTemplate.find(q, Category::class.java)
     }
 }
