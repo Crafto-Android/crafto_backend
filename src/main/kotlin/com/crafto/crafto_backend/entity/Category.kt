acb7870381c2906a99f5af0a781de4f6d772a65b
Hend Sayed <hendsayed853@gmail.com>
2025-10-21 18:10:59 +0300

    remove unused import

--- src/main/kotlin/com/crafto/crafto_backend/entity/Category.kt
+++ src/main/kotlin/com/crafto/crafto_backend/entity/Category.kt
@@ -1,6 +1,5 @@
 package com.crafto.crafto_backend.entity
 import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.index.TextIndexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document("categories")
